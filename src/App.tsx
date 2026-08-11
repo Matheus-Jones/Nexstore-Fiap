be5d54c4017806989bd4c0ed38f03934f1ec59c2
Matheus-Jones <matheus.jones@icloud.com>
2026-08-11 20:02:44 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './Button'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Button.tsx
@@ -0,0 +1,9 @@
+// tsx => typescript + xhtml
+// jsx => javascript + xhtml
+
+export function Button () {
+
+    return (
+        <button>clique aqui</button>
+    )
+}
