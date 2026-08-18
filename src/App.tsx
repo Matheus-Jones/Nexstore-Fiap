a5f6b5d8519431b3031edbdc316403e7db7b998f
Matheus-Jones <matheus.jones@icloud.com>
2026-08-18 20:10:29 -0300

    feat: added cart itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react"
 import { ProductCard } from "./components/ProductCard"
 import type { Product } from "./types/product"
+import type { CartItem } from "./types/cartitem"
 
 interface AppProps {
   products: Product[]
@@ -7,15 +9,40 @@ interface AppProps {
 
 function App(props: AppProps) {
 
+  const [cartItem, setCartItem] = useState <CartItem[]>([])
+
+  function handleAddToCart(product: Product):void {
+
+    const exists = props.products.find((item) => {
+      return item.id === product.id
+    })
+
+    console.log (exists)
+
+    if (exists) {
+
+      const cartItem: CartItem = {
+        product: exists,
+        quantity: 1
+      }
+      
+      setCartItem([cartItem])
+    }
+
+    console.log (cartItem)
+
+  }
+
 
   return (
     <main>
 
       <h1>NexStore</h1>
 
+
       {
         props.products.map((value) => {
-          return <ProductCard product={value} />
+          return <ProductCard product={value} onAddCart={handleAddToCart}/>
 
         })
       
